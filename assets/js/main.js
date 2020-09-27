d30c3c885c53f92574637d20c54b2d97459c7d37
orlando1979 <orlando1979@outlook.com>
2020-09-26 23:17:31 -0500

    26-09-2020 se elimino el archivo idetconfig

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,47 +1,47 @@
 /*===== MENU SHOW Y HIDDEN =====*/
-const navMenu = document.getElementById("nav-menu"),
-    toggleMenu = document.getElementById("nav-toggle"),
-    closeMenu = document.getElementById("nav-close");
+const navMenu = document.getElementById('nav-menu'),
+	toggleMenu = document.getElementById('nav-toggle'),
+	closeMenu = document.getElementById('nav-close');
 
 // SHOW
-toggleMenu.addEventListener("click", () => {
-    navMenu.classList.toggle("show");
+toggleMenu.addEventListener('click', () => {
+	navMenu.classList.toggle('show');
 });
 
 // HIDDEN
-closeMenu.addEventListener("click", () => {
-    navMenu.classList.remove("show");
+closeMenu.addEventListener('click', () => {
+	navMenu.classList.remove('show');
 });
 
 /* ==== REMOVE MENU ==== */
-const navLink = document.querySelectorAll(".nav__link");
+const navLink = document.querySelectorAll('.nav__link');
 
 function linkAction() {
-    navMenu.classList.remove("show");
+	navMenu.classList.remove('show');
 }
-navLink.forEach((n) => n.addEventListener("click", linkAction));
+navLink.forEach((n) => n.addEventListener('click', linkAction));
 
 /*===== SCROLL SECTIONS ACTIVE LINK =====*/
-const sections = document.querySelectorAll("section[id]");
+const sections = document.querySelectorAll('section[id]');
 
-window.addEventListener("scroll", scrollActive);
+window.addEventListener('scroll', scrollActive);
 
 function scrollActive() {
-    const scrollY = window.pageYOffset;
+	const scrollY = window.pageYOffset;
 
-    sections.forEach(current => {
-        const sectionHeight = current.offsetHeight;
-        const sectionTop = current.offsetTop - 50;
-        sectionId = current.getAttribute("id");
+	sections.forEach((current) => {
+		const sectionHeight = current.offsetHeight;
+		const sectionTop = current.offsetTop - 50;
+		sectionId = current.getAttribute('id');
 
-        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
-            document
-                .querySelector('.nav__menu a[href*=' + sectionId + ']')
-                .classList.add('active');
-        } else {
-            document
-                .querySelector('.nav__menu a[href*=' + sectionId + ']')
-                .classList.remove('active');
-        }
-    });
+		if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+			document
+				.querySelector('.nav__menu a[href*=' + sectionId + ']')
+				.classList.add('active');
+		} else {
+			document
+				.querySelector('.nav__menu a[href*=' + sectionId + ']')
+				.classList.remove('active');
+		}
+	});
 }
